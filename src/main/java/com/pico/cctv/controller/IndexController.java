862107ecc6e11d64d2a972fa00e77cdb88ab9eaf
atascg01 <atascg01@estudiantes.unileon.es>
2020-04-20 20:19:31 +0200

    Implemented Controller and Service classes

--- src/main/java/com/pico/cctv/controller/IndexController.java
+++ src/main/java/com/pico/cctv/controller/IndexController.java
@@ -1,6 +1,10 @@
 package com.pico.cctv.controller;
 
+import com.pico.cctv.domain.Camera;
+import com.pico.cctv.service.CameraSvc;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     
+    private final CameraSvc cameraSvc;
+
+    public IndexController(CameraSvc cameraSvc) {
+        this.cameraSvc = cameraSvc;
+    }
+    
     @RequestMapping("/")
-    public String renderIndex(){
+    public String renderIndex(Model model){
+        List<Camera> cameras = cameraSvc.findCameras();
+        model.addAttribute("cameras", cameras);
         return "index";
     }
 }
---
+++ src/main/java/com/pico/cctv/domain/Camera.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pico.cctv.domain;
+
+/**
+ *
+ * @author atascg01
+ */
+public class Camera {
+    private String name;
+    private String description;
+    private String urlImage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+    
+}
